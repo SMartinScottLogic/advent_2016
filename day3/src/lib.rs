9ec4f8ac30a1b3fc830191b84290bf11fda402c7
Simon Martin <havvoric@gmail.com>
2024-12-17 20:49:28 +0000

    Day 3 part 1 complete

--- day3/src/lib.rs
+++ day3/src/lib.rs
@@ -1,12 +1,19 @@
 use std::io::{BufRead, BufReader};
+use itertools::Itertools;
 #[allow(unused_imports)]
 use tracing::{debug, event_enabled, info, Level};
 
 pub type ResultType = u64;
 
 #[derive(Debug, Default)]
-pub struct Solution {}
-impl Solution {}
+pub struct Solution {
+    triangles: Vec<(ResultType, ResultType, ResultType)>,
+}
+impl Solution {
+    fn add_triangle(&mut self, a: ResultType, b: ResultType, c: ResultType) {
+        self.triangles.push((a, b, c));
+    }
+}
 
 #[allow(unused_variables, unused_mut)]
 impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
@@ -16,6 +23,8 @@ impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
         let mut solution = Self::default();
         for (id, line) in reader.lines().map_while(Result::ok).enumerate() {
             // Implement for problem
+            let (a, b, c) = line.split_whitespace().map(|v| v.parse::<ResultType>().unwrap()).next_tuple().unwrap();
+            solution.add_triangle(a, b, c);
         }
         Ok(solution)
     }
@@ -25,8 +34,12 @@ impl utils::Solution for Solution {
     fn analyse(&mut self, _is_full: bool) {}
 
     fn answer_part1(&self, _is_full: bool) -> Self::Result {
+        let r = self.triangles.iter().filter(|(a, b, c)| {
+            *a < (*b + *c) && *b < (*a + *c) && *c < (*a + *b)
+        }).count() as ResultType;
+        
         // Implement for problem
-        Ok(0)
+        Ok(r)
     }
 
     fn answer_part2(&self, _is_full: bool) -> Self::Result {

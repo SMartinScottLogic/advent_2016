f85a69aa2e37e91a9358b42732e46673b0c4b091
Simon Martin <havvoric@gmail.com>
2024-12-18 03:21:01 +0000

    Day 5 complete

--- day5/src/lib.rs
+++ day5/src/lib.rs
@@ -1,12 +1,18 @@
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Read};
 #[allow(unused_imports)]
 use tracing::{debug, event_enabled, info, Level};
 
-pub type ResultType = u64;
+pub type ResultType = String;
 
 #[derive(Debug, Default)]
-pub struct Solution {}
-impl Solution {}
+pub struct Solution {
+    door_id: String,
+}
+impl Solution {
+    fn set_doorid(&mut self, door_id: String) {
+        self.door_id = door_id;
+    }
+}
 
 #[allow(unused_variables, unused_mut)]
 impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
@@ -16,6 +22,7 @@ impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
         let mut solution = Self::default();
         for (id, line) in reader.lines().map_while(Result::ok).enumerate() {
             // Implement for problem
+            solution.set_doorid(line);
         }
         Ok(solution)
     }
@@ -25,30 +32,61 @@ impl utils::Solution for Solution {
     fn analyse(&mut self, _is_full: bool) {}
 
     fn answer_part1(&self, _is_full: bool) -> Self::Result {
+        let mut r = String::new();
+        let mut i = 0;
+        let mut found = 0;
+        loop {
+            let md5 = get_md5(&self.door_id, i);
+            if &md5[..5] == "00000" {
+                let ch = md5.chars().nth(5).unwrap();
+                debug!(i, ?ch);
+                r.push(ch);
+                found += 1;
+                if found == 8 {
+                    break;
+                }
+            }            
+            i += 1;
+        }
         // Implement for problem
-        Ok(0)
+        Ok(r)
     }
 
     fn answer_part2(&self, _is_full: bool) -> Self::Result {
+        let mut r = Vec::new();
+        r.resize(8, '_');
+        println!("{}", r.iter().collect::<String>());
+
+        let mut i = 0;
+        loop {
+            let md5 = get_md5(&self.door_id, i);
+            if &md5[..5] == "00000" {
+                let pos = md5.chars().nth(5).unwrap().to_digit(16).unwrap() as usize;
+                let ch = md5.chars().nth(6).unwrap();
+                debug!(?r, i, ?ch, pos);
+                if pos < 8 && r[pos] == '_' {
+                    r[pos] = ch;
+                    println!("{}", r.iter().collect::<String>());
+                    if r.iter().all(|c| *c!='_') {
+                        break;
+                    }
+                }
+            }            
+            i += 1;
+        }
         // Implement for problem
-        Ok(0)
+        Ok(r.iter().collect())
     }
 }
 
-#[cfg(test)]
-mod test {
-    use super::*;
-    use std::io::BufReader;
-
-    use tracing_test::traced_test;
-    use utils::Solution;
-
-    #[test]
-    #[traced_test]
-    fn read() {
-        let input = "replace for problem";
-        let r = BufReader::new(input.as_bytes());
-        let s = crate::Solution::try_from(r).unwrap();
-        assert_eq!(0 as ResultType, s.answer_part1(false).unwrap());
-    }
+fn get_md5(prefix: &str, value: i32) -> String {
+    let trial = format!("{}{}", prefix, value);
+    let md5 = md5::compute(&trial);
+    debug!(value, md5=?md5);
+    md5.bytes().flatten()
+    .take(4)
+    .fold(String::new(), |mut acc, v| {
+        acc.push_str(&format!("{:02x}", v));
+        acc
+    })
 }
